9df2dd07e47143d388ce52c61c7199e776acf80e
Armando Garcia Chavez <armandogarciachavez@192.168.1.67>
2026-02-09 13:05:23 -0600

    Fix: Agregando la ruta correcta para la API del Widget

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+// IMPORTANTE: Traemos el controlador que sí existe
+use App\Http\Controllers\Api\ContentController; 
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/feed/{project_id}', [\App\Http\Controllers\API\WidgetController::class, 'index']);
-Route::get('/v1/projects/{uuid}/articles', [\App\Http\Controllers\Api\ArticleController::class, 'index']);
+// --- ESTA ES LA RUTA QUE FALTABA ---
+Route::get('/v1/widget/{uuid}', [ContentController::class, 'getArticles']);
